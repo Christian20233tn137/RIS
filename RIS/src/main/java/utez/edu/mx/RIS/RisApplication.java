b1026bf1bf9c2241e78c8dffe2c62e2278f821fd
Chris <20233tn137@utez.edu.mx>
2025-01-08 12:52:25 -0600

    primer commit

---
+++ RIS/src/main/java/utez/edu/mx/RIS/RisApplication.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.RIS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RisApplication.class, args);
+	}
+
+}
